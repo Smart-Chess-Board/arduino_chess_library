c8eb031ada7dce3133782190ee5fcc763469e6b9
Adam Wei <adam.wei@mail.utoronto.ca>
2021-08-17 00:33:10 -0400

    added files for utility functions

---
+++ utility.cpp
@@ -0,0 +1,38 @@
+#include "utility.h"
+
+// coordinate conversions
+char intToFileUpper(int file){
+    return "A"-1 + file;
+}
+
+char intToFileLower(int file){
+    return "a"-1 + file;
+}
+
+String getCoordUpper(int file, int rank){
+    return intToFileUpper(file) + String(rank);
+}
+
+String getCoordLower(int file, int rank){
+    return intToFileLower(file) + String(rank);
+}
+
+// pieces
+String pieceName(PieceType type){
+    if (type == pawn) return "pawn";
+    else if (type == rook) return "rook";
+    else if (type == knight) return "knight";
+    else if (type == bishop) return "bishop";
+    else if (type == queen) return "queen";
+    else if (type == king) return "king";
+    else return "<none>";
+}
+
+String pieceName(const Piece& piece){
+    if (piece.color == white)
+        return "white " + pieceName(piece.type);
+    else if (piece.color == black)
+        return "black " + pieceName(piece.type);
+    else
+        return "no piece";
+}
---
+++ utility.h
@@ -0,0 +1,19 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include "enum.h"
+#include "Arduino.h"
+#include "piece.h"
+
+// coordinate conversions
+char intToFileUpper(int file);
+char intToFileLower(int file);
+String getCoordUpper(int file, int rank);
+String getCoordLower(int file, int rank);
+
+// pieces
+String pieceName(PieceType type);
+String pieceName(const Piece& piece);
+
+
+#endif
